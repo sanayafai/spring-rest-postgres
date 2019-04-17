5400f3ca514a24a796fec3f1228a748f70fdc333
Sana Yafai <sanayafai89@gmail.com>
2019-04-17 17:11:40 +0530

    first commit

---
+++ src/main/java/com/javasampleapproach/springrest/postgresql/SpringFoxConfig.java
@@ -0,0 +1,25 @@
+package com.javasampleapproach.springrest.postgresql;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.javasampleapproach.springrest.postgresql"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+}
